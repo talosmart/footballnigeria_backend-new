7ee22cbbdc0978b24a8c61e8edabcb62ebace353
Abdulrasak Abdulkadir <abdulrasakabdulkadir@yahoo.com>
2025-07-03 09:10:53 +0100

    [Feat] added implemetation for external api

---
+++ app/Http/Controllers/ExternalApi/RefreesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\ExternalApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ExternalApi\base;
+
+class RefreesController extends Controller
+{
+    function getRefrees(Request $request)
+    {
+        $base = new base();
+        $url = 'enter url here'; // Replace with the actual URL to fetch areas data
+        
+        return $base->fetchData($url);
+    }
+}
